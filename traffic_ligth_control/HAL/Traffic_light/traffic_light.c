52026558cf1786e7070aab9cce71399761589682
Hosam Aboabla <hosamaboabla@yahoo.com>
2022-08-14 12:58:10 +0200

    Update traffic_light.c
    add next function

--- traffic_ligth_control/HAL/Traffic_light/traffic_light.c
+++ traffic_ligth_control/HAL/Traffic_light/traffic_light.c
@@ -12,7 +12,7 @@ void traffic_light_init(traffic_light* tl)
 	LED_Initialization(&tl->green);
 	LED_Initialization(&tl->yellow);
 	LED_Initialization(&tl->red);
-	tl->status = traffic_status_red;
+	stop(tl);
 }
 
 void stop(traffic_light* tl)
@@ -21,6 +21,7 @@ void stop(traffic_light* tl)
 	LED_Off(&tl->green);
 	LED_Off(&tl->yellow);
 	tl->status = traffic_status_red;
+	// tl->previous_status = traffic_status_yellow;
 }
 void go(traffic_light* tl)
 {
@@ -28,11 +29,45 @@ void go(traffic_light* tl)
 	LED_Off(&tl->red);
 	LED_Off(&tl->yellow);
 	tl->status = traffic_status_green;
+	// tl->previous_status = traffic_status_red;
 }
 void ready(traffic_light* tl)
 {
-	LED_On(&tl->yellow);
-	LED_Off(&tl->green);
+	
+	if( tl->status == traffic_status_green)
+	{
+		LED_On(&tl->green);
+	}else{
+		LED_Off(&tl->green);
+	}
+	
+	LED_Off(&tl->yellow);
 	LED_Off(&tl->red);
+	
+	tl->previous_status = tl->status;
 	tl->status = traffic_status_yellow;
+}
+
+
+void next_state(traffic_light* tl)
+{
+	if(tl->status == traffic_status_green)
+	{
+		ready(tl);
+	}
+	else if(tl->status == traffic_status_yellow)
+	{
+		if(tl->previous_status == traffic_status_green)
+		{
+			stop(tl);	
+		}
+		else{
+			go(tl);
+		}
+		
+	}
+	else if(tl->status == traffic_status_red)
+	{
+		ready(tl);
+	}
 }
